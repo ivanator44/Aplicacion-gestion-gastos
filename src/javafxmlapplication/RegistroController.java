8e3dfe7fe6d0ae3e9112952e96bae3fcb8049a36
Iguanator <imiguanator@protonmail.com>
2024-05-14 13:36:40 +0200

    Cambios en "avatares", "imagenes" y registro

--- src/javafxmlapplication/RegistroController.java
+++ src/javafxmlapplication/RegistroController.java
@@ -25,6 +25,8 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import model.Acount;
 import model.AcountDAOException;
@@ -91,6 +93,13 @@ public class RegistroController implements Initializable {
                         
         /*BooleanBinding validFields = Bindings.and(validEmail, validPassword)
                  .and(equalPasswords);*/
+        
+        nicknameTextField.focusedProperty().addListener((observable, oldValue, newValue)->{
+            if(!newValue){
+                checkEditNickname();
+            }
+        });
+        
         emailTextField.focusedProperty().addListener((observable, oldValue, newValue)->{
             if(!newValue){ //focus lost
                 checkEditEmail();
@@ -109,31 +118,61 @@ public class RegistroController implements Initializable {
             }
         });
         
-        imagenesComboBox.getItems().addAll("/avatares/hombre (1).png");
+        imagenesComboBox.getItems().addAll("/avatares/1.png", "/avatares/2.png",
+                "/avatares/3.png", "/avatares/4.png", "/avatares/5.png", 
+                "/avatares/6.png", "/avatares/7.png", "/avatares/8.png",
+                "/avatares/9.png", "/avatares/10.png", "/avatares/11.png",
+                "/avatares/default.png");
+        imagenesComboBox.setCellFactory(c -> new ImagenComboCell());
+        
+        imagenesComboBox.valueProperty().addListener((observable, oldValue, newValue)->{
+            // Obtener la ruta de la imagen seleccionada
+            String imagePath = newValue;
+            // Cargar la imagen y establecerla como avatar
+            Image imagen = new Image(getClass().getResourceAsStream(imagePath));
+            avatarImageView.setImage(imagen);
+            imagenesComboBox.setButtonCell(new ImagenTickComboCell());
+        });
     }    
 
     // Función del botón de registrar
     @FXML
     private void registrar(ActionEvent event) throws AcountDAOException, IOException {
-        JavaFXMLApplication.setRoot("Autenticacion");
         if (validNickname.get() && validEmail.get() && validPassword.get() && equalPasswords.get()) {
             Acount cuenta = Acount.getInstance();
             String nombre = nombreTextField.getText();
             String apellido = apellidoTextField.getText();
             String email = emailTextField.getText();
-            String login = emailTextField.getText(); // Nickname
-            String contraseña = passwordField.getText(); // Nickname
-            //Image avatar = ima
-            //cuenta.registerUser(nombre, apellido, email, login, contraseña, image, LocalDate.MAX);
+            String login = emailTextField.getText(); // (Nickname) 
+            String contraseña = passwordField.getText();
+            
+            Image avatar;
+            if (avatarImageView.getImage() != null){
+                avatar = avatarImageView.getImage();
+            }else{ // S no se ha seleccionado ninguna imagen...
+                avatar = new Image(getClass().getResourceAsStream("/avatares/default.png"));
+            }
+            
+            cuenta.registerUser(nombre, apellido, email, login, contraseña, avatar, LocalDate.MAX);
+            JavaFXMLApplication.setRoot("Autenticacion");
         }
     }
 
     @FXML
     private void hyperlinkFuncion(ActionEvent event) {
-        JavaFXMLApplication.setRoot("Registro.fxml");
+        JavaFXMLApplication.setRoot("Autenticacion");
     }
     
     //-----------------------------------------------
+    // Métodos para comprobar que los campos son correctos
+    
+    private void checkEditNickname(){
+        if (nicknameTextField.getText().equals("")){
+            manageError(nicknameErrText, nicknameTextField, validNickname);
+        }else{
+            manageCorrect(nicknameErrText, nicknameTextField, validNickname);
+        }
+    }
     
     private void checkEditEmail(){
         if (!checkEmail(emailTextField.textProperty().getValueSafe())){
@@ -189,6 +228,8 @@ public class RegistroController implements Initializable {
         hideErrorMessage(errorLabel,textField);  
     }
     
+    //----------------------------------------------------
+    
     /**
      * Changes to red the background of the edit and
      * makes the error label visible
@@ -209,5 +250,46 @@ public class RegistroController implements Initializable {
     private void hideErrorMessage(Label errorLabel,TextField textField){
         errorLabel.visibleProperty().set(false);
         textField.styleProperty().setValue("");
+    }
+    
+    //--------------------------------------------------------------
+    
+    // Clase para mostrar las imágenes correctamente
+    class ImagenComboCell extends ComboBoxListCell<String> {
+        private ImageView view = new ImageView();
+        private Image imagen;
+        
+        @Override
+        public void updateItem(String t, boolean bln) {
+            super.updateItem(t, bln); //To change body of generated methods, choose Tools | Templates.
+            if (t == null || bln) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                imagen = new Image(t, 25, 25, true, true);
+                view.setImage(imagen);
+                setGraphic(view);
+            }
+        }
+    }
+    
+    // Clase para mostrar en la celda del comboBox cuando se seleccione alguna imagen
+    class ImagenTickComboCell extends ComboBoxListCell<String> {
+        private ImageView view = new ImageView();
+        private Image imagen;
+        
+        @Override
+        public void updateItem(String t, boolean bln) {
+            super.updateItem(t, bln); //To change body of generated methods, choose Tools | Templates.
+            if (t == null || bln) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                t = "/imagenes/tick.png";
+                imagen = new Image(t, 25, 25, true, true);
+                view.setImage(imagen);
+                setGraphic(view);
+            }
+        }
     }
 }
